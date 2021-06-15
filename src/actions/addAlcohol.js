f1da6ce997e90f749e69e2c4e90135f0b0e366aa
catprice444 <catprice444@gmail.com>
2021-06-15 16:46:04 -0400

    update form

---
+++ src/actions/addAlcohol.js
@@ -0,0 +1,6 @@
+export function addAlcohol(data) {
+    debugger;
+    return (dispatch) => {
+
+    }
+}
--- src/components/AlcoholInput.js
+++ src/components/AlcoholInput.js
@@ -1,4 +1,6 @@
 import React from 'react' 
+import {connect} from 'react-redux'
+import {addAlcohol} from '../actions/addAlcohol'
 
 class AlcoholInput extends React.Component{
     state = {name: '', website: '', preference: ''}
@@ -8,20 +10,27 @@ class AlcoholInput extends React.Component{
             [event.target.name]: event.target.value
         })
     }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addAlcohol(this.state)
+    } 
     
     render(){
         return(
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                    <label>Alcohol Name:</label><input type='text' value={this.state.name} name='name' onChange={this.handleChange}></input><br></br>
                 
-                   <label>Alcohol Website:</label><input type='url' value={this.state.website} name='website' onChange={this.handleChange}></input><br></br>
+                   <label>Alcohol Website:</label><input type='text' value={this.state.website} name='website' onChange={this.handleChange}></input><br></br>
 
-                   <label>Alcohol Preference:</label><input type='number' min='1' value={this.state.preference} name='preference' onChange={this.handleChange}></input> 
+                   <label>Alcohol Preference:</label><input type='number' min='1' value={this.state.preference} name='preference' onChange={this.handleChange}></input><br></br> 
+                
+                <input type='submit'></input>
                 </form>
             </div>
         )
     }
 }
 
-export default AlcoholInput
+export default connect(null, {addAlcohol})(AlcoholInput)
