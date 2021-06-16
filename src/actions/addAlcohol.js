13efeeb60c8337de89efff4552d4117548c1c1bb
catprice444 <catprice444@gmail.com>
2021-06-16 11:27:32 -0400

    working form and post requests

--- src/actions/addAlcohol.js
+++ src/actions/addAlcohol.js
@@ -1,6 +1,16 @@
 export function addAlcohol(data) {
-    debugger;
-    return (dispatch) => {
 
-    }
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/alcohols', {
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(alcohol => dispatch({type: 'ADD_ALCOHOL', payload: alcohol}))
+      }
+    
 }
