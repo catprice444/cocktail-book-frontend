65c38baa1634a76c0abad2c66039e56436531999
catprice444 <catprice444@gmail.com>
2021-06-16 23:38:48 -0400

    added Cocktails Container

--- src/components/Alcohol.js
+++ src/components/Alcohol.js
@@ -1,11 +1,13 @@
 import React from 'react';
-// import {Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const Alcohol = (props) => {
     let alcohol = props.alcohols.filter(alcohol => alcohol.id == props.match.params.id)[0]
     return(
         <li>
-            {alcohol ? alcohol.name : null} - {alcohol ? alcohol.website : null}
+            {alcohol ? alcohol.name : null} - 
+            {alcohol ? <a href='${alcohol.website}'/> : null}
+            {/* Fix the link */}
         </li>
     )
 }
---
+++ src/containers/CocktailsContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class CocktailsContainer extends React.Component {
+    render(){
+        return(
+            <div>
+                CocktailsContainer
+            </div>
+        )
+    }
+
+}
+
+export default CocktailsContainer
