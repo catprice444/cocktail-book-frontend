8f843b1358cb6f0e464760db8b1ab0e52aab6b78
catprice444 <catprice444@gmail.com>
2021-06-16 23:43:52 -0400

    added cocktail input

--- src/components/Alcohol.js
+++ src/components/Alcohol.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import CocktailsContainer from '../containers/CocktailsContainer'
 
 const Alcohol = (props) => {
     let alcohol = props.alcohols.filter(alcohol => alcohol.id == props.match.params.id)[0]
     return(
-        <li>
-            {alcohol ? alcohol.name : null} - 
-            {alcohol ? <a href='${alcohol.website}'/> : null}
+        <div>
+        <h2>
+            {alcohol ? alcohol.name : null} 
+            {/* {alcohol ? <a href='${alcohol.website}'/> : null} */}
             {/* Fix the link */}
-        </li>
+        </h2>
+        <CocktailsContainer/>
+        </div>
     )
 }
 
