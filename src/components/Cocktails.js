d234c2c5a4348c3e1a3065b22934520e03534fd6
catprice444 <catprice444@gmail.com>
2021-06-17 22:54:06 -0400

    added rating to cocktails page

--- src/components/Cocktails.js
+++ src/components/Cocktails.js
@@ -6,7 +6,8 @@ const Cocktails = (props) => {
 
             {props.cocktails && props.cocktails.map(cocktail => 
             <div key={cocktail.id}> 
-            <h3><u> Cocktail: {cocktail.name}</u> </h3>
+            <h3><u> Cocktail: {cocktail.name}</u> </h3> 
+            <p><b>{cocktail.rating}/5 Stars</b></p>
 
             <p><i>Ingredients:</i> {cocktail.ingredients}</p>
 
