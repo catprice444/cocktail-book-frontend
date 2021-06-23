c8fdd81196ea729794986b2dadb92c24ebf8d974
catprice444 <catprice444@gmail.com>
2021-06-23 18:06:05 -0400

    deleting edit function

--- src/containers/CocktailsContainer.js
+++ src/containers/CocktailsContainer.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import CocktailInput from '../components/CocktailInput'
 import Cocktails from '../components/Cocktails'
+import {Route, Switch} from 'react-router-dom';
+import Cocktail from '../components/Cocktail';
 
 class CocktailsContainer extends React.Component {
+  
+
     render(){
         return(
             <div>
-                <CocktailInput alcohol={this.props.alcohol}/>
-                <Cocktails cocktails={this.props.alcohol && this.props.alcohol.cocktails}/>
+                {/* <Switch> */}
+                    <CocktailInput alcohol={this.props.alcohol}/>
+                    {/* <Route path={`alcohols/${alcohol.id}/cocktail/${cocktail.id}/edit`} render={(routerProps) => <Cocktail {...routerProps} alcohols={this.props.alcohols.id} cocktail={this.props.alcohols.cocktail}/>}/> */}
+                    {/* <Route path='/cocktails/:id' render={(routerProps) => <Cocktail {...routerProps} cocktails={this.props.alcohols.cocktails}/>}/> */}
+                    <Cocktails cocktails={this.props.alcohol && this.props.alcohol.cocktails}/>
+                {/* </Switch> */}
             </div>
         )
     }
-
 }
+
+
 
 export default CocktailsContainer
--- src/reducers/alcoholReducer.js
+++ src/reducers/alcoholReducer.js
@@ -3,7 +3,7 @@ export default function alcoholReducer(state = {alcohols: []}, action){
     switch (action.type) {
         case 'FETCH_ALCOHOLS':
             return {alcohols: action.payload}
-
+    
         case 'ADD_ALCOHOL':
             return {...state, alcohols: [...state.alcohols, action.payload]}
 
@@ -24,15 +24,6 @@ export default function alcoholReducer(state = {alcohols: []}, action){
                         return alcohol
                     }
                 })}
-
-        case 'EDIT_COCKTAIL':
-            return{...state, alcohols: state.alcohols.map(alcohol => {
-                if (alcohol.id === action.payload.id){
-                    return action.payload
-                } else {
-                    return alcohol
-                }
-            })}
 
         default: 
             return state
