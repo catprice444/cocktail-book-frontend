c66170182cbba659a8c7214dac261568ee886549
catprice444 <catprice444@gmail.com>
2021-06-23 22:55:51 -0400

    adding a way to see all the cocktails listing'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
     return(
         <div>
-            <Link to='/' style={{paddingRight: '10px'}}> Home</Link>
+            <Link to='/cocktails' style={{paddingRight: '10px'}}> Cocktails</Link>
             <Link to='/alcohols' style={{paddingRight: '10px'}}>Alcohols  </Link>
             <Link to='/alcohols/new' style={{paddingRight: '10px'}}> Add Alcohol</Link>
         </div>
