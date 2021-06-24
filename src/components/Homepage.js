8d61d1d5e0c258afa752fc79f7bd70598878a6de
catprice444 <catprice444@gmail.com>
2021-06-24 01:36:19 -0400

    working homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 
 const Homepage = (props) => {
     return(
         <div>
-            Welcome to the Cocktail Book! 
-            format this, add a logo and
+            <h1>Welcome To Your Cocktail Book! </h1>
+            
+            <h3>List of current cocktails</h3>
+
+           {props.cocktails.map(cocktail => 
+            <ol key={cocktail.id}>
+                <b><Link to={`/alcohols/${cocktail.alcohol_id}`}>{cocktail.name}</Link></b> <br></br>
+                Rating: {cocktail.rating}/5 stars
+            </ol>
+            )}
         </div>
     )
 }
