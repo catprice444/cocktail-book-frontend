c3b12a4347296927c25b195b5501f5457363c0c3
catprice444 <catprice444@gmail.com>
2021-06-23 22:17:15 -0400

    working cocktail page

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -5,7 +5,13 @@ const Cocktail = (props) => {
     let cocktail = props.cocktails.filter(cocktail => cocktail.id == props.match.params.id)[0]
     return(
         <div>
-        {cocktail ? cocktail.name :null}
+        <h2>{cocktail.name}</h2>
+        
+        <p><b>{cocktail.rating}/5 Stars</b></p>
+
+        <p><i>Ingredients:</i> {cocktail.ingredients}</p>
+
+        <p><i>Recipe:</i> {cocktail.recipe}</p>
         </div>
     )
 }
